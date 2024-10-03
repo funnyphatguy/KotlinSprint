e82b49f742d940b94a524113a6bf17a3aadbde8f
funnyphatguy <contius32@gmail.com>
2024-10-03 22:56:31 +0300

    KS-1-4 create new variables (Float to Double).(String to Long)

--- src/main/kotlin/lesson1/lesson_1_task_4.kt
+++ src/main/kotlin/lesson1/lesson_1_task_4.kt
@@ -1,11 +1,11 @@
 package org.example.lesson1
 
 fun main() {
-    val length: String = "40_868_600_000"
+    val length: Long = 40_868_600_000L
     val age: Byte = 27
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    val partOfYear: Float = 2.0547945205479453E-4f
+    val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
 
     print("$length \n$age \n$partOfDay \n$seconds \n$partOfYear \n$apogee")
