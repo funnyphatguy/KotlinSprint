1a60652c4f9ac8d2743c85afb27a4540b811739a
funnyphatguy <contius32@gmail.com>
2024-10-04 17:45:07 +0300

    KS-1-1 made variables

---
+++ src/main/kotlin/lesson2/lesson_2 task_1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson2
+
+fun main() {
+
+    val firstStudent = 3
+    val secondStudent = 4
+    val thirdStudent = 3
+    val fourthStudent = 5
+
+    val overallNumberOfStudents = 4f
+
+    val averageValue = (firstStudent + secondStudent + thirdStudent + fourthStudent) / overallNumberOfStudents
+
+    println(averageValue)
+}
