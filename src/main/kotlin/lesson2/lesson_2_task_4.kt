66b54056eef7945fd516e6b4152f488bc6a2833c
funnyphatguy <contius32@gmail.com>
2024-10-06 23:43:57 +0300

    KS-2-4 buff correction, empty lines deleted

--- src/main/kotlin/lesson2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson2/lesson_2_task_4.kt
@@ -1,16 +1,14 @@
 package org.example.lesson2
 
-const val BUFF = 0.2
+const val BUFF = 20
 
 fun main() {
 
     val chrystalOre = 7f
     val metalOre = 11f
 
-
-    val chrystaOreBuff = chrystalOre * BUFF
-    val metalOreBuff = metalOre * BUFF
-
+    val chrystaOreBuff = chrystalOre / 100 * BUFF
+    val metalOreBuff = metalOre / 100 * BUFF
 
     println("Бонусная кристалическая руда ${chrystaOreBuff.toInt()}")
     println("Бонусная железная руда ${metalOreBuff.toInt()}")
