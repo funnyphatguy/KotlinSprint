dc3095b3f7a474f923df699ccd5552a30aa8f2e4
funnyphatguy <contius32@gmail.com>
2024-10-06 23:00:03 +0300

    KS-1-4 deleting unnecessary files

--- src/main/kotlin/lesson1/lesson_1 task1.kt
+++
@@ -1,9 +0,0 @@
-package org.example.lesson1
-
-fun main () {
-    val numberOfOrders: Int = 75
-    val thankForYourPurchase: String = "Спасибо за покупку, возвращайтесь снова!"
-
-    println("Количество заказов $numberOfOrders")
-    println(thankForYourPurchase)
-}
