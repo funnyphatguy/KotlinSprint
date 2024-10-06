e1543b25aecdf93067f4614d031ca5d4781ef43b
funnyphatguy <contius32@gmail.com>
2024-10-06 18:06:22 +0300

    KS-3-1 initial commit

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+
+fun main() {
+
+    val name = "Алешка"
+    val greetingDay = "Добрый день"
+    val greetingEvening = "Добрый вечер"
+
+    println("$greetingDay, $name")
+    println("$greetingEvening, $name")
+
+}
