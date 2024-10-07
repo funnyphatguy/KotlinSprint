e04b33c203745e2b95a1bffa0438a06513fd5c3f
funnyphatguy <contius32@gmail.com>
2024-10-07 19:52:09 +0300

    KS-3-2 family and age initialization changed

--- src/main/kotlin/lesson3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson3/lesson_3_task_2.kt
@@ -1,17 +1,15 @@
 
-
 fun main() {
     val name = "Татьяна"
     val patronymic = "Сергеевна"
-    val family = "Сидорова"
-    val age = 22
+    var family = "Андреева"
+    var age = 20
 
+    println("ФИО: $family $name $patronymic, возраст: $age")
 
+    family = "Сидорова"
+    age = 22
 
-    val familyOld = "Андреева"
-    val ageOld = 20
-
-    println("ФИО: $familyOld $name $patronymic, возраст: $ageOld")
     println("ФИО: $family $name $patronymic, возраст: $age")
 
 }
