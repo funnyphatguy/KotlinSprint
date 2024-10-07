c6c7446afe74a00d73f1978786847177c9276b58
funnyphatguy <contius32@gmail.com>
2024-10-07 21:38:11 +0300

    KS-3-4 first commit

---
+++ src/main/kotlin/lesson3/lesson_3 task_4.kt
@@ -0,0 +1,15 @@
+
+fun main () {
+
+    var from = "E2"
+    var to = "E4"
+    var turnNumber = 1
+
+    println("$from-$to;$turnNumber")
+
+    from = "D2"
+    to = "D3"
+    turnNumber = 2
+
+    println("$from-$to;$turnNumber")
+}
