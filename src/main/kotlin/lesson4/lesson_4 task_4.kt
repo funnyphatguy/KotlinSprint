e26ac85aae2a7589603024069a18806e1dc877f0
funnyphatguy <contius32@gmail.com>
2024-10-09 22:08:51 +0300

    KS-4-4 second commit

--- src/main/kotlin/lesson4/lesson_4 task_4.kt
+++ src/main/kotlin/lesson4/lesson_4 task_4.kt
@@ -1,19 +1,19 @@
 fun main() {
 
-    val arms = 5
-    val press = 5
-    val legs = 6
-    val back = 6
+    var arms = 5
+    var press = 5
+    val legs = arms++
+    val back = press++
 
-    val day_of_the_train = 6
+    val day_of_the_train = 5
 
     println(
         """
-        Упражнения для рук:    ${day_of_the_train == arms}    
-        Упражнения для ног:    ${day_of_the_train == legs} 
-        Упражнения для спины:  ${day_of_the_train == back}  
+        Упражнения для рук:    ${day_of_the_train == arms}
+        Упражнения для ног:    ${day_of_the_train == legs}
+        Упражнения для спины:  ${day_of_the_train == back}
         Упражнения для пресса: ${day_of_the_train == press}
-        
+
     """.trimIndent()
     )
 }
