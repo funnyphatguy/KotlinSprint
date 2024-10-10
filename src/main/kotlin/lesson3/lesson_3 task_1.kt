b98a69ff36c98a97c96d02d882e0c37dcd4c85f7
funnyphatguy <contius32@gmail.com>
2024-10-10 17:54:04 +0300

    KS-3-1 first commit

---
+++ src/main/kotlin/lesson3/lesson_3 task_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson3
+
+fun main() {
+
+    val name = "Алешка"
+    var greeting: String
+
+    greeting = "Добрый день, $name"
+    println(greeting)
+
+    greeting = "Добрый вечер, $name"
+    println(greeting)
+
+}
