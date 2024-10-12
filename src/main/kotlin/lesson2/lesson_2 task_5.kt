2d3db1cb4bd14b2c6f8bb5033f7f06208992c9ce
funnyphatguy <contius32@gmail.com>
2024-10-12 21:43:41 +0300

    KS-2-5 1.167 to 16.7

--- src/main/kotlin/lesson2/lesson_2 task_5.kt
+++ src/main/kotlin/lesson2/lesson_2 task_5.kt
@@ -6,9 +6,9 @@ const val FIRST_PAY = 70000
 
 fun main() {
 
-    val percents = 1.167
+    val percents = 16.7
     val years = 20
 
-    val fV = FIRST_PAY * (percents).pow(years)
+    val fV = FIRST_PAY * ((percents / 100) + 1).pow(years)
     println("%.3f".format(fV))
 }
