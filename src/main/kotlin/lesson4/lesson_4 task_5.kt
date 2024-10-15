71ff5199b2b690c9e5c46b1abe34c1b5622b24d4
funnyphatguy <contius32@gmail.com>
2024-10-15 20:55:56 +0300

    KS-4-5 добавил: новые константы, метод lowercase()

--- src/main/kotlin/lesson4/lesson_4 task_5.kt
+++ src/main/kotlin/lesson4/lesson_4 task_5.kt
@@ -3,11 +3,13 @@ package org.example.lesson4
 const val PEOPLE_MIN = 50
 const val PEOPLE_MAX = 70
 const val PROVISION = 50
+const val YES_ANSWER = "Да"
+const val NO_ANSWER = "Нет"
 
 fun main() {
 
     println("Корабль имеет повреждения?(да либо нет)")
-    val damage: String = readln()
+    val damage: String = readln().lowercase()
 
     println("Какое количество людей на борту?")
     val people: Int = readln().toInt()
@@ -16,11 +18,11 @@ fun main() {
     val provision: Int = readln().toInt()
 
     println("Благоприятны ли метеоусловия?(да либо нет)")
-    val weather: String = readln()
+    val weather: String = readln().lowercase()
 
-    val canFlyOrNot = (damage == "да") || (damage != "нет") and (people >= PEOPLE_MIN) and
-            (people <= PEOPLE_MAX) and (provision > PROVISION) and
-            (weather == "да") || (weather != "нет")
+    val canFlyOrNot =
+        (damage != YES_ANSWER.lowercase()) and (people >= PEOPLE_MIN) and
+                (people <= PEOPLE_MAX) and (provision > PROVISION) and (weather != NO_ANSWER.lowercase())
 
     println("Может ли судно полететь? $canFlyOrNot")
 }
