83e94a9aa528688181dd3b0010f958ace84deeaf
funnyphatguy <contius32@gmail.com>
2024-10-17 10:34:07 +0300

    KS-6-2 убрал лишний цикл

--- src/main/kotlin/lesson6/lesson_6 task_2.kt
+++ src/main/kotlin/lesson6/lesson_6 task_2.kt
@@ -3,13 +3,9 @@ package org.example.lesson6
 fun main() {
 
     println("Введите количество секунд, которые нужно засечь")
-    var counter = readln().toInt()
-    val strike = counter
+    val counter = readln().toInt()
 
-    while (counter > 0) {
-        println("${counter--}")
-        Thread.sleep(1000)
-    }
-    println("Прошло секунд: $strike")
-    Thread.sleep(90000000)   //Это она якобы встает на паузу. Абсолютный костыль, но как сделать по другому не понимаю.
+    Thread.sleep(1000L*counter)
+
+    println("Прошло секунд: $counter")
 }
