18e82dbc4358b615138a97983d73668296674a15
funnyphatguy <contius32@gmail.com>
2024-10-26 11:32:01 +0300

    KS-11-3 сделал все в одном файл и исправил код

--- src/main/kotlin/lesson_11_3/lesson_11_3.kt
+++ src/main/kotlin/lesson_11_3/lesson_11_3.kt
@@ -1,33 +1,71 @@
-package org.example.lesson_11_3
+const val STATUS_TALKING = "Разговаривает"
+const val STATUS_MUTED = "Пользователь заглушен"
+const val STATUS_OFF = "Микрофон выключен"
+const val NICKNAME_NEW = "АЛЕШКА"
+const val NICKNAME_ANNA = "Анна"
+const val NICKNAME_PETER = "Петр"
+const val NICKNAME_ALEKSANDER = "Александр"
+
+class Room(
+
+    val cover: String,
+    val name: String,
+    val users: MutableList<User>
+
+) {
+    fun addToChat(user: User): MutableList<User> {
+        users.add(user)
+        return users
+    }
+
+    fun findUserAndChangeStatus(username: String, status: String) {
+        val user = users.find { it.nickname == username }
+        user?.status = status
+    }
+
+}
+
+class User(
+    val nickname: String,
+    var status: String
+) {
+    override fun toString(): String {
+        return "$nickname, ($status)"
+    }
+}
+
+
+
+
 
 fun main() {
 
     var redrum = Room(
-        cover = "Музыкальная группа",
-        name = "Рок",
-        users = mutableListOf("Аня", "Артем", "Марк")
+        cover = "Обложка - музыкальная группа",
+        name = "Рок-группа",
+        users = mutableListOf(
+            User(NICKNAME_ANNA, STATUS_TALKING),
+            User(NICKNAME_PETER, STATUS_MUTED),
+            User(NICKNAME_ALEKSANDER, STATUS_OFF)
+        )
     )
 
     var newUser = User(
-        nickname = "Алешка",
+        nickname = NICKNAME_NEW,
         status = STATUS_OFF
     )
 
+    println("Экран приложения:")
     println(redrum.name)
     println(redrum.cover)
     println(redrum.users)
-
+    println("Новый пользователь заходит")
     println(newUser.nickname)
     println(newUser.status)
 
-    redrum.addToChat(nickname = NICKNAME)
 
-    println(redrum.name)
-    println(redrum.cover)
+    println("Новый пользователь попал в чат")
+    redrum.findUserAndChangeStatus(NICKNAME_NEW, STATUS_TALKING)
+
     println(redrum.users)
-
-    newUser.addedUserStatus(nickname = NICKNAME, status = STATUS_TALKING)
-
-    println(newUser.nickname)
-    println(newUser.status)
 }
