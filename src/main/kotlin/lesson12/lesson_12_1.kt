1a351e5d3b775ed9b6012a02a7e3f11b73b94d8d
funnyphatguy <contius32@gmail.com>
2024-10-28 22:00:04 +0300

    KS-12-1 убрал лишние комментарии

--- src/main/kotlin/lesson12/lesson_12_1.kt
+++ src/main/kotlin/lesson12/lesson_12_1.kt
@@ -1,40 +1,5 @@
 package org.example.lesson12
 
-//class person(val name: String, val weght: Int, val count: Int) {
-//
-//    var isNeedToPrepare: Boolean = false
-//
-//    constructor(
-//        name: String,
-//        weght: Int,
-//        count: Int,
-//        isNeedToPrepare: Boolean,
-//    ) : this(name, weght, count) {
-//        this.isNeedToPrepare = isNeedToPrepare
-//    }
-//}
-//
-//fun main() {
-//
-//    val alesha = person(name = "Алешка", 8, count = 5)
-//    val petya = person(name = "Петя", weght = 6, count = 3, isNeedToPrepare = true)
-//}
-
-//Программа отслеживает погоду по дням. Создай класс для хранения данных о температуре и осадках для одного дня.
-//
-//Объекты класса будут хранить:
-//
-//- дневная температура;
-//- ночная температура;
-//- наличие осадков в течение суток.
-//
-//Создай два объекта, присвой им произвольные данные и выведи в консоль одним методом созданного класса.
-//
-//- в классе создай переменные и проинициализируй их какими-нибудь значениями;
-//- затем в созданных объектах подставь другие значения этим переменным.
-//
-//В этой задаче используй пустой конструктор.
-
 class Temperature() {
 
     var dayTemp: Int = 0
