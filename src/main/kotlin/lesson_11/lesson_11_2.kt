fc868c9d3e8f0fcfe662265591e3cc5770a46cb8
funnyphatguy <contius32@gmail.com>
2024-10-28 10:52:03 +0300

    KS-11-2 сделал bio необязательным для заполнения, удалил отдельный файл с классом, теперь все в одном файле.

--- src/main/kotlin/lesson_11/lesson_11_2.kt
+++ src/main/kotlin/lesson_11/lesson_11_2.kt
@@ -1,6 +1,44 @@
-package org.example.lesson_11
+package org.example.lesson_11_1
 
-import org.example.lesson_11_1.User2
+
+class User2(
+
+    val id: Int,
+    val login: String,
+    var password: Int,
+    val email: String,
+    var bio: String = "расскажите о себе"
+
+) {
+
+    fun info() {
+        println("Идентификационный номер: $id; \nЛогин: $login; \n" +
+                "Пароль: $password; \n" +
+                "Адрес электронной почты: $email; \n" +
+                "Биография:$bio.")
+    }
+
+    fun bioChange(): String {
+        println("Расскажите немного о себе:")
+        bio = readln()
+        return bio
+    }
+
+    fun passwordChange() {
+        println("Введите пароль:")
+        val tryPassword = readln().toInt()
+        if (password == tryPassword) {
+            println("Введите новый пароль:")
+        } else {
+            println("Пароль введен неверно")
+            return
+        }
+        var newPassword = readln().toInt()
+        password = newPassword
+        println("Пароль успешно изменен!")
+
+    }
+}
 
 fun main() {
 
@@ -9,9 +47,10 @@ fun main() {
         login = "Artem",
         password = 67497,
         email = "Artem@yandex.ru",
-        bio = ""
-
     )
+
+    println("Информация о пользователе:")
+
     user3.info()
 
     user3.bioChange()
@@ -21,4 +60,5 @@ fun main() {
     user3.passwordChange()
 
     user3.info()
+
 }
