501e81d87e16b3dc40a30b732c5a9210749de348
funnyphatguy <contius32@gmail.com>
2024-10-29 16:10:56 +0300

    KS-12-3 first commit

---
+++ src/main/kotlin/lesson12/lesson_12_3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson12
+
+class DayTemperaturee(
+    val KelvinTemp: Int
+) {
+    var dayTemp: Int = KelvinTemp - 273
+    val nightTemp: Int = KelvinTemp - 273
+    val precipitation: Boolean = false
+
+
+    fun info() =
+        println("Дневная температура : $dayTemp градусов\nНочная температура : $nightTemp градусов,\nНаличие осадок :$precipitation")
+}
+
+fun main() {
+
+    val monday = DayTemperaturee(270)
+    monday.dayTemp = 36
+    monday.info()
+}
