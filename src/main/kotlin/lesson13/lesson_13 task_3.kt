54bb28c0c2112ddf227fcb85afbc05a782b812c5
funnyphatguy <contius32@gmail.com>
2024-10-30 18:17:22 +0300

    KS-13-3 first commit

---
+++ src/main/kotlin/lesson13/lesson_13 task_3.kt
@@ -0,0 +1,38 @@
+package org.example.lesson13
+//Скопируй класс из предыдущей задачи и измени его название для избегания конфликтов.
+//
+//Для того, чтобы взаимодействовать со всеми записями телефонной книги, как с одним целым, их нужно объединить в список.
+//
+//- создай и инициализируй (одновременно) список с 5 объектами класса-контакта;
+//- 2 контакта должны принимать null для поля компании;
+//- 1 контакт должен иметь название компании “null”;
+//- выведи список всех существующих компаний, которые есть в телефонной книге.
+
+class Contact(val name: String?, val number: Long?, val company: String?) {
+
+    fun getInfo() {
+
+        println(
+            """  
+               |- Имя : $name
+               |- Номер: $number
+               |- Компания: ${company ?: "<не указано>"}""".trimMargin()
+        )
+    }
+}
+
+fun main() {
+
+    val data = Contact(name = "Aleksei", number = 44542624, company = null)
+
+    val contacts = mutableListOf(
+        Contact(name = "Артем", number = 5634543, company = "Газпром"),
+        Contact(name = "Егор", number = 98675467, company = "null"),
+        Contact(name = "Петр", number = 46276354, company = null),
+        Contact(name = "Елисей", number = 57365435, company = null),
+        Contact(name = "Златояр", number = 9747542, company = "Майкрософт"),
+    )
+
+    contacts.forEach { it -> it.getInfo() }
+
+}
