ebe98d0a5181ba5d6bfa149a7cd2b38b948a7dff
funnyphatguy <contius32@gmail.com>
2024-11-04 20:52:36 +0300

    KS-15-2 убрал интерфейс, теперь метод отправки сообщения принимает объект типа WeatherStationStats.

--- src/main/kotlin/lesson_15/lesson_15 task_2.kt
+++ src/main/kotlin/lesson_15/lesson_15 task_2.kt
@@ -4,48 +4,34 @@ package org.example.lesson_15
 //
 //Создай абстрактный класс WeatherStationStats. Он будет служить родителем для классов-объектов погодных данных: Temperature и PrecipitationAmount.
 //
-//Реализуй логику имитации отправки сообщения на сервер в классе WeatherServer. Метод отправки сообщения должен принимать объект типа WeatherStationStats и,
+//Реализуй логику имитации отправки сообщения на сервер в классе WeatherServer.
+// Метод отправки сообщения должен принимать объект типа WeatherStationStats и,
 // в зависимости от типа переданного объекта, выводить соответствующие данные.
 //
 //Создай по одному объекту для разных типов погодных данных и протестируй работу программы.
 
-abstract class WeatherStationStats(val data: Double) {
-
-    override fun toString(): String {
-        return "$data"
-    }
+abstract class WeatherStationStats() {
+    abstract val data: Double
 }
+class Temperature(override val data: Double) : WeatherStationStats() {}
 
-class Temperature(data: Double) : WeatherStationStats(data) {}
+class PrecipitationAmount(override val data: Double) : WeatherStationStats() {}
 
-class PrecipitationAmount(data: Double) : WeatherStationStats(data) {}
-
-interface Rec {
-    fun tempReciever(temperature: Temperature) {
-        println("Значение температуры: ${temperature.data}")
-    }
-
-    fun PrecipitationReciever(precipitationAmount: PrecipitationAmount) {
-        println("Значение осадков: ${precipitationAmount.data}")
-    }
-
-}
-
-class WeatherServer() : Rec {
-    override fun tempReciever(temperature: Temperature) {
-        super.tempReciever(temperature)
-
-    }
-
-    override fun PrecipitationReciever(precipitationAmount: PrecipitationAmount) {
-        super.PrecipitationReciever(precipitationAmount)
+class WeatherServer() {
+    fun sendMessage(weather: WeatherStationStats) {
+        when (weather) {
+            is Temperature -> println("Температура ${weather.data} градуса")
+            is PrecipitationAmount -> println("Количество осадков ${weather.data} мм")
+        }
     }
 }
 
 fun main() {
 
     val temperature = Temperature(data = 14.6)
+    val precipitation = PrecipitationAmount(data = 22.0)
 
-    val server = WeatherServer().tempReciever(temperature)
-
+    val server = WeatherServer()
+    server.sendMessage(temperature)
+    server.sendMessage(precipitation)
 }
