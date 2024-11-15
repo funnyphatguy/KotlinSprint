cebbd86d7851b0b7615229322d15005b27ecd6ca
funnyphatguy <contius32@gmail.com>
2024-11-15 19:28:23 +0300

    KS-20-2 first commit

---
+++ src/main/kotlin/lesson_20/lesson_20 task_2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_20
+
+class Player(val name: String, var health: Int, var maxHealth: Int) {
+
+    val healthPotion: (Player) -> Unit = { health = maxHealth }
+
+}
+
+fun main() {
+
+    val player = Player("Алешка", 50, 100)
+    println(player.health)
+    player.healthPotion(player)
+    println(player.health)
+
+
+}
