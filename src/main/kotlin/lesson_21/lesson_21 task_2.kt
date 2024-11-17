5fbe697e813445b3a8aebbf091c34423c758b49b
funnyphatguy <contius32@gmail.com>
2024-11-17 21:45:45 +0300

    KS-21-2 first commit

---
+++ src/main/kotlin/lesson_21/lesson_21 task_2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_21
+
+fun List<Int>.evenNumbersSum(): Int {
+
+    val newList: MutableList<Int> = mutableListOf()
+    for (i in this)
+        if (i % 2 == 0)
+            newList.add(i)
+    return newList.sum()
+}
+
+fun main() {
+
+    val simpleList: List<Int> = listOf(4, 6, 2, 7, 9, 10, 18, 4)
+
+
+    println("Сумма четных чисел в массиве: ${simpleList.evenNumbersSum()}")
+
+}
