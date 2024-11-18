ec53eeb48cb49941dc92eea532f8b1aa3fbb550d
funnyphatguy <contius32@gmail.com>
2024-11-18 20:41:09 +0300

    KS-21-2 изменил функцию evenNumbersSum

--- src/main/kotlin/lesson_21/lesson_21 task_2.kt
+++ src/main/kotlin/lesson_21/lesson_21 task_2.kt
@@ -3,10 +3,9 @@ package org.example.lesson_21
 fun List<Int>.evenNumbersSum(): Int {
 
     val newList: MutableList<Int> = mutableListOf()
-    for (i in this)
-        if (i % 2 == 0)
-            newList.add(i)
-    return newList.sum()
+
+    return this.filter { it % 2 == 0 }.sum()
+
 }
 
 fun main() {
