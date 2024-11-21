cd1c3055bca344388e6f44c81f1eef0d3b87abad
funnyphatguy <contius32@gmail.com>
2024-11-21 21:59:04 +0300

    KS-22-4 first commit

---
+++ src/main/kotlin/lesson_22/lesson_22 task_4.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_22
+
+data class MainScreenState(
+    val data: String? = null,
+    val isLoading: Boolean = false
+)
+
+class MainScreenModel() {
+
+    var mainScreenState: MainScreenState = MainScreenState()
+
+    val newState = mainScreenState
+
+    fun setNewState(newState: MainScreenState) {
+
+        mainScreenState = newState
+        mainScreenState = newState.copy(isLoading = true)
+        mainScreenState = newState.copy(data = "Data", isLoading = true)
+
+    }
+
+}
+
+
+
+
+
+
+
+
