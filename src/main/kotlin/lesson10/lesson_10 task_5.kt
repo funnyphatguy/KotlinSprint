8a6b92c1e51f3b38f1caad44c773a20dae51062b
funnyphatguy <contius32@gmail.com>
2024-11-23 12:13:48 +0300

    KS-10-5 убрал все методы из main, поменял названия

--- src/main/kotlin/lesson10/lesson_10 task_5.kt
+++ src/main/kotlin/lesson10/lesson_10 task_5.kt
@@ -3,42 +3,41 @@ package org.example.lesson10
 const val LOGIN = "АЛЕШКА"
 const val PASSWORD = 22233004
 
+fun generateToken(): String {
+
+    val chars = 'a'..'z'
+    val numbers = '1'..'9'
+    val bigChars = 'A'..'Z'
+
+    val token = mutableListOf<Char>()
+    val overallChars = chars + numbers + bigChars
+
+    for (i in 0..32) {
+        token.add(overallChars.random())
+        token.shuffle()
+    }
+    val stringToken = token.joinToString("")
+    return stringToken
+}
+
+fun autorization(login: String, password: Int): String? {
+    if (login == LOGIN && PASSWORD == password) {
+        return generateToken()
+    } else {
+        return null
+    }
+}
+
+fun getBasket(value: String?) {
+    when (value) {
+        is String -> println("В вашей корзине сало и пиво")
+        null -> println("Неверная авторизация")
+    }
+}
+
 fun main() {
 
-    fun toCreateMassive(): String {
+    val user = autorization(LOGIN, PASSWORD)
 
-        val chars = 'a'..'z'
-        val numbers = '1'..'9'
-        val bigChars = 'A'..'Z'
-
-        val token = mutableListOf<Char>()
-        val overallChars = chars + numbers + bigChars
-
-        for (i in 0..32) {
-            token.add(overallChars.random())
-            token.shuffle()
-        }
-        val stringToken = token.joinToString("")
-        return stringToken
-    }
-
-    fun checker(login: String, password: Int): String? {
-        if (login == LOGIN && PASSWORD == password) {
-            val token = toCreateMassive()
-            return token
-        } else {
-            val token = null
-            return token
-        }
-    }
-
-    fun basket() {
-        val value = checker(LOGIN, PASSWORD)
-
-        when (value) {
-            is String -> println("В вашей корзине сало и пиво")
-            null -> println("Неверная авторизация")
-        }
-    }
-    basket()
+    getBasket(user)
 }
