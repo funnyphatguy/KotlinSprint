51e10a4badf375f857a9fbbb61cc01e1667c89b2
funnyphatguy <contius32@gmail.com>
2024-11-25 21:45:29 +0300

    KS-22-5 добавил реализацию через компонентные функции

--- src/main/kotlin/lesson_22/lesson_22 task_5.kt
+++ src/main/kotlin/lesson_22/lesson_22 task_5.kt
@@ -13,14 +13,20 @@ fun main() {
         placeName = "Альфа центавра",
         placeDescription = "Скопление звезд",
         dateAndTime = "20.04.2789 года в 14:32 по Москве",
-        distance = 4.3
+        distance = 4.3,
     )
+
+    val name = alphaCentauri.component1()
+    val description = alphaCentauri.component2()
+    val date = alphaCentauri.component3()
+    val distance = alphaCentauri.component4()
+
     println(
         """
-        |Название места или события: ${alphaCentauri.placeName}
-        |Описание места или события: ${alphaCentauri.placeDescription}
-        |Дата и время события: ${alphaCentauri.dateAndTime}
-        |Расстояние места или события от Земли в световых годах: ${alphaCentauri.distance}
+        |Название места или события: $name
+        |Описание места или события: $description
+        |Дата и время события: $date
+        |Расстояние места или события от Земли в световых годах: $distance
     """.trimMargin()
     )
 
