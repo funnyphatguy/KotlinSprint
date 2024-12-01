4190bea35b8904c72450fb637d4b708fdfe79c43
funnyphatguy <contius32@gmail.com>
2024-12-01 22:27:40 +0300

    KS-18-5 теперь методов шесть

--- src/main/kotlin/lesson_18/lesson_18 task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18 task_5.kt
@@ -2,20 +2,38 @@ package org.example.lesson_18
 
 class Screen() {
 
-    fun draw(x: Int, y: Float): String {
-        if ((x == 0) || (y in 0.1f..100.0f)) {
+    fun drawCircle(x: Int, y: Int): String {
+        if ((x == 0) || (y in 1..100)) {
             return "Вы нарисовали круг с центром в координате х=$x и радиусом $y"
         } else return "Вам не удалось нарисовать круг"
     }
 
-    fun draw(x: Int, y: Int): String {
+    fun drawCircle(x: Float, y: Float): String {
+        if ((x == 0.0f) || (y in 0.1f..100.0f)) {
+            return "Вы нарисовали круг с центром в координате х=$x и радиусом $y"
+        } else return "Вам не удалось нарисовать круг"
+    }
+
+    fun drawPoint(x: Int, y: Int): String {
         if ((x == 0) || (y == 0) && (x == y)) {
             return "Вы нарисовали точку в координатах х=$x и y=$y"
         } else return "Вам не удалось поставить точку, проверьте, чтобы координаты совпадали"
     }
 
-    fun draw(x: Float, y: Float): String {
+    fun drawPoint(x: Float, y: Float): String {
+        if ((x == 0.0f) || (y == 0.0f) && (x == y)) {
+            return "Вы нарисовали точку в координатах х=$x и y=$y"
+        } else return "Вам не удалось поставить точку, проверьте, чтобы координаты совпадали"
+    }
+
+    fun drawSquare(x: Float, y: Float): String {
         if ((x in 5.0f..100.0f) || (y in 5f..100.0f) && (x == y)) {
+            return "Вы нарисовали квадрат со сторонами х=$x и $y"
+        } else return "Вам не удалось нарисовать квадрат, проверьте, чтобы координаты совпадали"
+    }
+
+    fun drawSquare(x: Int, y: Int): String {
+        if ((x in 5..100) || (y in 5..100) && (x == y)) {
             return "Вы нарисовали квадрат со сторонами х=$x и $y"
         } else return "Вам не удалось нарисовать квадрат, проверьте, чтобы координаты совпадали"
     }
@@ -24,6 +42,6 @@ class Screen() {
 fun main() {
 
     val fugure = Screen()
-    fugure.draw(5.0f, 5.0f).also { println(it) }
+    fugure.drawCircle(5.0f, 5.0f).also { println(it) }
 
 }
