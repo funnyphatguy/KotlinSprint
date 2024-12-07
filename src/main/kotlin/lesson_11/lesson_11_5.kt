1aadce0327896640afc508083aec4bbcb3e5f25c
funnyphatguy <contius32@gmail.com>
2024-12-07 19:00:41 +0300

    KS-11-5 переписал код

--- src/main/kotlin/lesson_11/lesson_11_5.kt
+++ src/main/kotlin/lesson_11/lesson_11_5.kt
@@ -1,76 +1,51 @@
 package org.example.lesson_11
 
-//Реализовать класс работы форума (Forum).
-// Для реализации создай дополнительные классы для сущностей “Член форума” (с полями userId и userName)
-// и “Сообщение форума” (с полями authorId и message).
-//
-//Примени паттерн “Строитель” для создания пользователей и сообщений форума.
-// Напрямую объекты не создаются, логика должна быть инкапсулирована внутри методов класса.
-//
-//- createNewUser() создает новых пользователей, принимая имя пользователя.
-// Метод сохраняет нового пользователя в общий список и одновременно возвращает новый объект.
-// Генерация id-шников новых пользователей происходит внутри;
-//- createNewMessage() создает сообщения, принимая id пользователя.
-// Сообщения создаются только если такой пользователь есть на форуме;
-//-  печатает в консоль все сообщения добавленные на форум в формате:
-//
-//    автор: сообщение
-//    автор: сообщение
-//
-//Создай экземпляр Forum и продемонстрируй его работу, добавив двух пользователей и по два сообщения от каждого пользователя.
-
-
-class ForumMember(val userId: Int, userName: String) {
-
-    class Builder {
-
-        var userName: String = ""
-
-        fun createNewUser(setName: String): ForumMember {
-
-            apply { this.userName = setName }
-            val id = (1..2).random()
-            val newUser = ForumMember(id, setName)
-            listOfForumMembers.add(newUser)
-            return newUser
-        }
-    }
-}
-
-var listOfForumMembers: MutableList<ForumMember> = mutableListOf()
-var listOfForumMessages: MutableList<String> = mutableListOf()
-
-class ForumMessage(authorId: Int, message: String) {
-
-    class Builder {
-
-        val authorId = 0
-
-        fun createNewMessage(id: Int) {
-            val id = authorId
-            for (ForumMember in listOfForumMembers) {
-                if (ForumMember.userId == id) {
-
-                    var messages: MutableList<String> =
-                        mutableListOf("Привет, я тут новичок", "Привет, я модератор", "Привет, я тут старичок")
-                    val message = messages.random()
-                    listOfForumMessages.add(message)
-                } else println("Такого пользователя на форуме не существует")
-            }
-        }
-    }
-}
+import java.util.UUID
 
 class Forum() {
 
+    val listOfForumUsers: MutableList<ForumUser> = mutableListOf()
+    val listOfForumMessages: MutableList<ForumMessage> = mutableListOf()
+
+    fun createNewUser(name: String): ForumUser {
+
+        val newUser = ForumUser(userId = UUID.randomUUID().toString(), userName = name)
+        listOfForumUsers.add(newUser)
+        return newUser
+
+    }
+
+    fun createNewMessage(userId: String, message: String) {
+
+        val user = listOfForumUsers.find { it.userId == userId }
+
+        val forumMessage = ForumMessage(authorId = userId, message = message)
+        listOfForumMessages.add(forumMessage)
+        println("${user?.userName.toString()}: $message")
+
+    }
+
+   data class ForumUser(
+        val userId: String,
+        val userName: String
+    )
+
+   data class ForumMessage(
+        val authorId: String,
+        val message: String
+    )
 }
 
 fun main() {
 
-//    val person = ForumMember.Builder().createNewUser()
-//
-//    val message = ForumMessage.Builder().createNewMessage(1)
-//
-}
+    val forum = Forum()
 
+    val userVasyan = forum.createNewUser("Васян")
+    forum.createNewMessage(userVasyan.userId, "Надеюсь это правильное решение задачи")
+
+     val userValerka = forum.createNewUser("Валерка")
+    forum.createNewMessage(userValerka.userId, "сообщение")
+    forum.createNewMessage(userValerka.userId, "еще одно сообщение")
+
+}
 
